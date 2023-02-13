d49c5fe4214e2d0907016f9794e0cf23c4bef26d
zinoubm <zinne.eddine.2000@gmail.com>
2023-02-13 09:40:04 +0100

    finished setting up the candlestick graph

--- app.py
+++ app.py
@@ -4,11 +4,49 @@ import dash_daq as daq
 from dash.exceptions import PreventUpdate
 import requests
 from dotenv import load_dotenv
+import pandas as pd
+from dateutil import parser
+import plotly.graph_objects as go
+from plotly.graph_objs import Layout
+
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
+layout = {
+    "paper_bgcolor": "rgb(31, 41, 46)",
+    "plot_bgcolor": "rgba(0,0,0,0)",
+    "font_color": "rgb(255, 255, 255)",
+    "font": {
+        "family": "Courier New, monospace",
+        "size": 14,
+        "color": "#fff",
+    },
+}
 
 app = Dash(__name__)
+
+
+def generate_candlestick_chart(df):
+    layout = Layout(
+        paper_bgcolor="rgb(31, 41, 46)",
+        plot_bgcolor="rgba(0,0,0,0)",
+        font_color="rgb(255, 255, 255)",
+        font={"family": "Courier New, monospace", "size": 14, "color": "#fff"},
+    )
+    fig = go.Figure(
+        data=[
+            go.Candlestick(
+                x=df.index,
+                open=df["open"],
+                high=df["high"],
+                low=df["low"],
+                close=df["close"],
+            )
+        ],
+        layout=layout,
+    )
+
+    return fig
 
 
 class Controller:
@@ -19,6 +57,7 @@ class Controller:
         self.api_key = None
         self.adjusted = None
         self.compact = None
+        self.data = None
         self.window = None
 
     def get_data(self):
@@ -31,11 +70,31 @@ class Controller:
             compact = "compact"
         else:
             compact = "full"
+
         url = f"https://www.alphavantage.co/query?function={self.function}&symbol={self.symbol}&interval={self.interval}min&apikey={self.api_key}&adjusted={adjusted}&outputsize={compact}"
         print(url)
         r = requests.get(url)
         data = r.json()
-        return data
+        self.data = self.preprocess_data(data)
+
+    def preprocess_data(self, df):
+        df = pd.DataFrame.from_dict(df["Time Series (5min)"], orient="index")
+        df = df.rename(
+            columns={
+                "1. open": "open",
+                "2. high": "high",
+                "3. low": "low",
+                "4. close": "close",
+                "5. volume": "vloume",
+            }
+        )
+
+        for col in df.columns:
+            df[col] = pd.to_numeric(df[col])
+
+        df_index = df.index.map(lambda x: parser.parse(x))
+        df.index = df_index
+        return df
 
     def __str__(self):
         return f"Class::Controller\nfunction::{self.function}\nsymbol::{self.symbol}\ninterval::{self.interval}\nadjusted::{self.adjusted}\noutputsize::{self.compact}\nwindow::{self.window}"
@@ -74,7 +133,7 @@ app.layout = html.Div(
                 dcc.Slider(
                     step=None,
                     marks={1: "1", 5: "5", 15: "15min", 30: "30min", 60: "60min"},
-                    value=15,
+                    value=5,
                     id="time-interval-slider",
                 ),
                 html.H6("Adjusted"),
@@ -88,7 +147,15 @@ app.layout = html.Div(
         ),
         html.Div(
             className="main",
-            children=[html.Div(id="out1"), html.Div(id="out2")],
+            children=[
+                html.Div(id="out1"),
+                html.Div(id="out2"),
+                # dcc.Graph(id="candle-candlestick-graph"),
+                dcc.Graph(
+                    figure={"layout": layout},
+                    id="candle-candlestick-graph",
+                ),
+            ],
         ),
     ],
 )
@@ -115,13 +182,15 @@ def set_app_controller_params(function, symbol, interval, adjusted, compact, win
 
 
 @app.callback(
-    Output("out2", "children"),
+    Output("candle-candlestick-graph", "figure"),
     Input("get-data", "n_clicks"),
 )
 def get_data(n_clicks):
     if n_clicks is None:
         raise PreventUpdate
-    return app_controller.get_data()
+    app_controller.get_data()
+
+    return generate_candlestick_chart(app_controller.data)
 
 
 if __name__ == "__main__":
